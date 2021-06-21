b30ba6adeb9f299d3a57ec5a935f82e7e0bcba16
21seu <1693376841@qq.com>
2021-06-21 22:40:54 +0800

    Runnable方式创建线程

--- 03_JUC/01_Create/src/main/java/pro03_runnable/RunnableTest01.java
+++ 03_JUC/01_Create/src/main/java/pro03_runnable/RunnableTest01.java
@@ -7,6 +7,15 @@ package pro03_runnable;
  *  3、创建实现类对象
  *  4、将此对象作为参数传递给Thread类的构造器中，创建Thread类的对象
  *  5、通过Thread类的对象去调用start()
+ *
+ *  比较线程创建的两种方式：
+ *  开发中优选选择实现Runnable接口方式
+ *  原因：
+ *      1、实现的方式没有类的单继承的局限性
+ *      2、实现的方式更适合来处理多个线程有共享数据的情况
+ *  联系：
+ *      1、Thread类实现了Runnable接口
+ *      2、相同点：两种方式都需要重写run()方法，将线程要是执行的逻辑都声明在run()方法中
  */
 public class RunnableTest01 {
     public static void main(String[] args) {
