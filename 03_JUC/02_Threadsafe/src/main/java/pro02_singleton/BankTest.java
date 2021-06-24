ffe02ab26ff7765456f2035aa6758fb423eb96df
21seu <1693376841@qq.com>
2021-06-24 23:31:41 +0800

    2021/6/24

--- 03_JUC/02_Threadsafe/src/main/java/pro02_singleton/BankTest.java
+++ 03_JUC/02_Threadsafe/src/main/java/pro02_singleton/BankTest.java
@@ -1,4 +1,34 @@
 package pro02_singleton;
 
+/**
+ * 使用同步机制将单例模式中的懒汉式改写为线程安全的
+ */
 public class BankTest {
+}
+
+class Bank {
+    private Bank() {
+
+    }
+
+    private static Bank instance = null;
+
+    public static Bank getInstance() {
+        //方式1：效率稍差
+        /*synchronized (Bank.class) {
+            if (instance == null){
+                instance = new Bank();
+            }
+            return instance;
+        }*/
+        //方式2：效率稍高
+        if (instance == null) {
+            synchronized (Bank.class) {
+                if (instance == null) {
+                    instance = new Bank();
+                }
+            }
+        }
+        return instance;
+    }
 }
