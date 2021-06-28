2658c450c0eff4f6129e441cb00e8a7e07d9b28e
21seu <1693376841@qq.com>
2021-06-28 23:35:54 +0800

    2021/06/28代码调整

--- 03_JUC/02_Threadsafe/src/main/java/pro05_ex/AccountTest.java
+++ 03_JUC/02_Threadsafe/src/main/java/pro05_ex/AccountTest.java
@@ -1,4 +1,50 @@
 package pro05_ex;
 
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 银行有一个账户
+ * 有两个储户分别向同一个账户存3000元，每次存1000，存3次，每次存完打印账户余额
+ * <p>
+ * 分析：
+ * 1、多线程问题 2、有共享数据 3、所以有线程安全问题  4、需要考虑线程安全问题怎么解决：三种方式
+ *
+ * @author ftj
+ */
 public class AccountTest {
+    private static double account = 0;
+
+    public static void main(String[] args) {
+        ReentrantLock lock = new ReentrantLock();
+        new Thread(() -> {
+
+            try {
+                lock.lock();
+                for (int i = 0; i < 3; i++) {
+                    account += 1000;
+                    Thread.sleep(1000);
+                    System.out.println("目前账户余额：" + account + "元，由账户1存进去的");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }, "储户1").start();
+        new Thread(() -> {
+            try {
+                lock.lock();
+                for (int i = 0; i < 3; i++) {
+                    account += 1000;
+                    Thread.sleep(1000);
+                    System.out.println("目前账户余额：" + account + "元，由账户2存进去的");
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }, "储户2").start();
+    }
 }
--- 03_JUC/02_Threadsafe/src/main/java/pro05_ex/AccountTest02.java
+++ 03_JUC/02_Threadsafe/src/main/java/pro05_ex/AccountTest02.java
@@ -1,4 +1,53 @@
 package pro05_ex;
 
+/**
+ * 银行有一个账户
+ * 有两个储户分别向同一个账户存3000元，每次存1000，存3次，每次存完打印账户余额
+ */
+class Account {
+    private double balance;
+
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
+    //存钱的方法
+    public synchronized void deposit(double amt) {
+        if (amt > 0) {
+            balance += amt;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "：存钱成功，余额为：" + balance);
+        }
+    }
+}
+
+class Customer extends Thread {
+
+    private Account account;
+
+    public Customer(Account account, String name) {
+        super(name);
+        this.account = account;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            account.deposit(100);
+        }
+    }
+}
+
 public class AccountTest02 {
+    public static void main(String[] args) {
+        Account account = new Account(0);
+        Customer c1 = new Customer(account,"甲");
+        Customer c2 = new Customer(account,"乙");
+        c1.start();
+        c2.start();
+    }
 }
