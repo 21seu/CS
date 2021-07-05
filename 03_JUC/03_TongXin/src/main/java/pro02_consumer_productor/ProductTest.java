5690e6222c078ac772bf25d02f119b886294662a
21seu <1693376841@qq.com>
2021-07-05 23:02:10 +0800

    生产者/消费者案例

--- 03_JUC/03_TongXin/src/main/java/pro02_consumer_productor/ProductTest.java
+++ 03_JUC/03_TongXin/src/main/java/pro02_consumer_productor/ProductTest.java
@@ -1,7 +1,104 @@
 package pro02_consumer_productor;
 
 /**
+ * 线程通信的应用：经典案例：生产者/消费者
+ * <p>
+ * 生产者将产品交给电源，而消费者从店员处取走产品，店员一次只能持有固定数量的产品，
+ * 如果生产者试图生成更多产品，电源会叫生产者停一下，如果店中有空位放产品了再通知生产者继续生产；
+ * 如果店中没有产品了，店员会告诉消费者等一下，如果店中有产品了再通知消费者来取走产品
+ * <p>
  * Created by fengtj on 2021/7/4 23:43
  */
+class Clerk {
+
+    private int productCount = 0;
+
+    /**
+     * 生产产品
+     */
+    public synchronized void produceProduct() {
+        if (productCount < 20) {
+            productCount++;
+            System.out.println(Thread.currentThread().getName() + "：开始生产第" + productCount + "个产品");
+            notify();
+        } else {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 消费产品
+     */
+    public synchronized void consumerProduct() {
+        if (productCount > 0) {
+            System.out.println(Thread.currentThread().getName() + "：开始消费第" + productCount + "个产品");
+            productCount--;
+            notify();
+        } else {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Producer extends Thread { //生产者
+    private Clerk clerk;
+
+    public Producer(Clerk clerk) {
+        this.clerk = clerk;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(getName() + "：开始生产产品....");
+        while (true) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            clerk.produceProduct();
+        }
+    }
+}
+
+class Consumer extends Thread {
+    private Clerk clerk;
+
+    public Consumer(Clerk clerk) {
+        this.clerk = clerk;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(getName() + "：开始消费产品....");
+        while (true) {
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            clerk.consumerProduct();
+        }
+    }
+}
+
 public class ProductTest {
+    public static void main(String[] args) {
+        Clerk clerk = new Clerk();
+        Producer p1 = new Producer(clerk);
+        p1.setName("生产者1");
+
+        Consumer c1 = new Consumer(clerk);
+        c1.setName("消费者1");
+        p1.start();
+        c1.start();
+    }
 }
